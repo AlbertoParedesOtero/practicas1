742e660d606f7fd0015255d8f2ce9ec087838322
Alberto <albertoparedes2196@gmail.com>
2022-08-16 16:34:21 +0200

    agrego bucle for

--- ZZZInformeNinja/src/librerias/NewClass.java
+++ ZZZInformeNinja/src/librerias/NewClass.java
@@ -13,6 +13,10 @@ public class NewClass {
     public static void main(String[] args) {
         
         System.out.println("hola mundo");
+        for(int i=0;i<10;i++){
+            System.out.println(i);
+        
+        }
         
     }
     
